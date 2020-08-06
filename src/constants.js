82d6da20ea16ab9fe387f0051d73c6f3c5f16b80
Jake Donahue <jdonahue135@gmail.com>
2020-08-06 12:51:48 -0400

    edit testimonials constant

--- src/constants.js
+++ src/constants.js
@@ -1,27 +1,25 @@
 import twitterPhoto from "./images/twitter.png";
 import simpsonsPhoto from "./images/simpsons.png";
 import battleshipPhoto from "./images/battleship.png";
+import fanPhoto from "./images/fan.jpeg";
+import paigePhoto from "./images/paige.jpeg";
+import connorPhoto from "./images/connor.jpeg";
 
 export const projects = {
   TWITTER_CLONE: {
     name: "Twitter Clone",
-    description:
-      "A build of Twitter's main functionality from the ground up. Using NodeJS/Express for the back end, MongoDB as database, and ReactJS for client side views. Deployed on Heroku, using AWS S3 to save user uploaded photos.",
     github: "https://github.com/jdonahue135/odin-twitter",
     live: "https://afternoon-fjord-78280.herokuapp.com/home",
     photo: twitterPhoto,
   },
   SIMPSONS_WALDO: {
     name: "Where's Waldo: The Simpsons",
-    description:
-      "A simple photo-tagging app. Find The Simpson's family in a large photo. Using node.js/Express as a backend and React.js as frontend framework.",
     github: "https://github.com/jdonahue135/wheres-waldo",
     live: "https://infinite-headland-08203.herokuapp.com/",
     photo: simpsonsPhoto,
   },
   BATTLESHIP: {
     name: "Battleship",
-    description: "A single-page app of the game Battleship.",
     github: "https://github.com/jdonahue135/battleship",
     live: "https://jdonahue135.github.io/battleship/",
     photo: battleshipPhoto,
@@ -30,21 +28,24 @@ export const projects = {
 
 export const testimonials = [
   {
-    name: "Fan",
+    name: "Fan Bi",
     title: "CEO, Blank Label",
     text:
-      "In a fast-moving world, it's great to have someone like Jake who has a demonstrated track record of being adaptive to new challenges. He created a brand new inside sales program from scratch that is now at the core of our business strategy. The best part is throughout all of this he was very proactive, resourceful and able to learn on his own.",
+      "In a fast-moving world, it's great to have someone like Jake who has a demonstrated track record of being adaptive to new challenges.",
+    photo: fanPhoto,
   },
   {
-    name: "Paige",
+    name: "Paige Connell",
     title: "President, Blank Label",
     text:
-      "Jake is a passionate, committed and detail-oriented professional. His performance was consistently top notch and I would not hesitate to work with him on any project big or small. His adaptability and problem solving capabilities made him a huge asset to our team. ",
+      "Jake's performance is consistently top notch and his adaptability and problem solving capabilities made him a huge asset to our team.",
+    photo: paigePhoto,
   },
   {
-    name: "Connor",
+    name: "Connor Lees",
     title: "Director of Operations, Blank Label",
     text:
-      "Strong, analytical problem-solving skills were the cornerstone of his successes. Extremely reliable and hard-working, and a great teammate to have in the office. Would recommend without hesitation.",
+      "Strong, analytical problem-solving skills were the cornerstone of his successes. Extremely reliable and hard-working, and a great teammate.",
+    photo: connorPhoto,
   },
 ];
