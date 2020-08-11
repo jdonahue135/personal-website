cc3d923bfd4640544086943dcea38ab373c38abd
Jake Donahue <jdonahue135@gmail.com>
2020-08-11 15:38:44 -0400

    add scrolling function to component and implement

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,30 @@
 import React from "react";
 
 const Navbar = (props) => {
+  const scrollTo = (e) => {
+    if (e.target.id) {
+      e.target.id === "about"
+        ? window.scrollTo(0, 570)
+        : window.scrollTo(0, 2000);
+    } else {
+      window.scrollTo(0, 0);
+    }
+  };
   return (
     <div className="navbar">
-      <div className="work link">Work</div>
-      <div className="logo">
+      <div className="about link" id="about" onClick={scrollTo}>
+        About
+      </div>
+      <div className="logo" onClick={scrollTo}>
         <div className="outer-circle">
           <div className="inner-circle">
             <div className="initials">JD</div>
           </div>
         </div>
       </div>
-      <div className="about link">About</div>
+      <div className="work link" id="work" onClick={scrollTo}>
+        Work
+      </div>
     </div>
   );
 };
