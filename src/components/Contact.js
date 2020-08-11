b879641cb7f4a75e9a305e538fa4caf46c0cdecc
Jake Donahue <jdonahue135@gmail.com>
2020-08-11 15:37:26 -0400

    add Contact component and implement

---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Contact = (props) => {
+  return (
+    <div className="contact-container">
+      <div className="contact-text">
+        Send an email about your project and I will get back to you within 24
+        hours.
+      </div>
+      <button className="contact-btn">Email me</button>
+    </div>
+  );
+};
+
+export default Contact;
