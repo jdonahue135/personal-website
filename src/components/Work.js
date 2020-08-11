cc0c4fecc1bf0f679f336e78a8babf0a1488dc8a
Jake Donahue <jdonahue135@gmail.com>
2020-08-11 15:38:16 -0400

    add Work component and implement

---
+++ src/components/Work.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import WorkItem from "./WorkItem";
+
+const Work = (props) => {
+  return (
+    <div className="work-container">
+      <div className="headline work-headline">Work Samples</div>
+      <WorkItem name="Twitter Clone" />
+      <WorkItem name="Simpsons Waldo" />
+      <WorkItem name="Battleship" />
+    </div>
+  );
+};
+
+export default Work;
