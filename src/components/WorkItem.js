19be08c416ca869fad25fb6d6a0c9b116aafc330
Jake Donahue <jdonahue135@gmail.com>
2020-08-11 15:39:21 -0400

    remove some content from component and change constant name to 'projects'

--- src/components/WorkItem.js
+++ src/components/WorkItem.js
@@ -1,27 +1,25 @@
 import React from "react";
-import { constants } from "../constants";
+import { projects } from "../constants";
 import githubImage from "../images/github.png";
 
 const WorkItem = (props) => {
   const formattedName = props.name.toUpperCase().replace(/ /, "_");
-  const project = constants[formattedName];
+  const project = projects[formattedName];
   return (
     <div className="work-item-container">
       <div className="image-container">
         <a href={project.live} target="_blank" rel="noopener noreferrer">
+          <div className="work-item-title" id={project.name}>
+            {project.name}
+          </div>
           <img src={project.photo} alt="project" />
         </a>
       </div>
-      <div className="work-item-about-container">
-        <a href={project.live} target="_blank" rel="noopener noreferrer">
-          <div className="work-item-title">{props.name}</div>
+      <div className="link-container">
+        <a href={project.github} target="_blank" rel="noopener noreferrer">
+          <img src={githubImage} alt="github" />
+          <p className="link-text">View on Github</p>
         </a>
-        <div className="work-item-description">{project.description}</div>
-        <div className="link-container">
-          <a href={project.github} target="_blank" rel="noopener noreferrer">
-            <img src={githubImage} alt="github" />
-          </a>
-        </div>
       </div>
     </div>
   );
