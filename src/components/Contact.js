f5514877882ffa31f4c12319621fc4d4897254d6
Jake Donahue <jdonahue135@gmail.com>
2020-08-12 12:35:27 -0400

    add copy to clipboard functionality and display message

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,41 @@
 import React from "react";
 
 const Contact = (props) => {
+  const handleClick = () => {
+    const email = document.querySelector(".email");
+    email.select();
+    document.execCommand("copy");
+    displayMessage();
+  };
+
+  const displayMessage = () => {
+    const message = document.querySelector("#message-" + props.id);
+    message.classList = message.classList + " show";
+    setTimeout(hideMessage, 1000);
+  };
+
+  const hideMessage = () => {
+    const message = document.querySelector("#message-" + props.id);
+    message.classList = "contact-message";
+  };
+
   return (
     <div className="contact-container">
       <div className="contact-text">
         Send an email about your project and I will get back to you within 24
         hours.
       </div>
-      <a href="mailto:jdonahue135@gmail.com">
-        <button className="contact-btn">Email me</button>
-      </a>
+      <div className="btn-container">
+        <a href="mailto:jdonahue135@gmail.com">
+          <button className="contact-btn">Send Email</button>
+        </a>
+        <button onClick={handleClick} className="contact-btn">
+          Copy Email
+        </button>
+        <div className="contact-message" id={"message-" + props.id}>
+          Email copied to clipboard.
+        </div>
+      </div>
     </div>
   );
 };
