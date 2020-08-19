afafafaf2fbe44544a13534822d0bff3b4cbdadd
Jake Donahue <jdonahue135@gmail.com>
2020-08-19 10:54:45 -0400

    remove scroll function and pass as prop from App component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,16 @@
 import React from "react";
 
 const Navbar = (props) => {
-  const scrollTo = (e) => {
-    if (e.target.id) {
-      e.target.id === "about"
-        ? window.scrollTo(0, 570)
-        : window.scrollTo(0, 2000);
-    } else {
-      window.scrollTo(0, 0);
-    }
-  };
   return (
     <div className="navbar">
-      <div className="about link" id="about" onClick={scrollTo}>
-        About
+      <div className="link" id="about" onClick={props.onClick}>
+        ABOUT
       </div>
-      <div className="logo" onClick={scrollTo}>
-        <div className="outer-circle">
-          <div className="inner-circle">
-            <div className="initials">JD</div>
-          </div>
-        </div>
+      <div className="link" id="experience" onClick={props.onClick}>
+        EXPERIENCE
       </div>
-      <div className="work link" id="work" onClick={scrollTo}>
-        Work
+      <div className="link" id="contact" onClick={props.onClick}>
+        CONTACT
       </div>
     </div>
   );
