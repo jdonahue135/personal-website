a461b4ce4bef7180e2e5266b82aac40c6947c7b2
Jake Donahue <jdonahue135@gmail.com>
2020-08-19 10:55:23 -0400

    change headline content

--- src/components/Work.js
+++ src/components/Work.js
@@ -5,7 +5,7 @@ import WorkItem from "./WorkItem";
 const Work = (props) => {
   return (
     <div className="work-container">
-      <div className="headline work-headline">Work Samples</div>
+      <div className="headline work-headline">My Work</div>
       <WorkItem name="Twitter Clone" />
       <WorkItem name="Simpsons Waldo" />
       <WorkItem name="Battleship" />
